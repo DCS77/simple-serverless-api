e90d6bc567f1b0681b5a34d6712cd6cb705b7e1c
Daniel Smit <dcsmit7@gmail.com>
2022-07-14 00:28:06 +0700

    Add fetching of name from query parameters

--- handler.js
+++ handler.js
@@ -2,15 +2,25 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 app.get("/", (req, res, next) => {
   return res.status(200).json({
-    message: "Hello from root!",
+    message: "Available endpoints: ['/cars']",
   });
 });
 
-app.get("/hello", (req, res, next) => {
+app.get("/cars", (req, res, next) => {
+  const {query} = req;
+
+  if(!query.name) {
+    return res.status(400).json({
+      error: "'name' wasn't specified in query parameters. Ensure name is sent in query body.",
+    });
+  }
+
   return res.status(200).json({
-    message: "Hello from path!",
+    message: `Searching for ${query.name}`,
   });
 });
 
