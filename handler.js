5d8c9386a8bcaf2b4e0ea381a4b501faab7fa5e7
Daniel Smit <dcsmit7@gmail.com>
2022-07-14 01:03:00 +0700

    Allow querying cars json and apply filtering

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
+const cars = require("./data/cars.json");
 
 app.use(express.json())
 
@@ -10,18 +11,24 @@ app.get("/", (req, res, next) => {
   });
 });
 
+function findQueryMatch({query, data}) {
+  // Search for all data items with fields matching query
+  return data.filter((item) =>
+    Object.keys(query).every((field) =>
+      query[field] === String(item[field])));
+}
+
 app.get("/cars", (req, res, next) => {
   const {query} = req;
 
-  if(!query.name) {
-    return res.status(400).json({
-      error: "'name' wasn't specified in query parameters. Ensure name is sent in query body.",
-    });
+  // Return all if no parameters entered
+  if(!Object.keys(query)) {
+    return res.status(200).json(cars);
   }
 
-  return res.status(200).json({
-    message: `Searching for ${query.name}`,
-  });
+  const searchResult = findQueryMatch({query, data: cars});
+
+  return res.status(200).json(searchResult);
 });
 
 app.use((req, res, next) => {
